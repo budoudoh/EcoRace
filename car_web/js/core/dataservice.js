2b13d58a45e33ad4d29660a26cac12cc384b77b3
Trevor Corcoran <trevor@trevcordev.com>
2015-01-04 02:17:53 -0500

    Initial commit for html web/mobile app
    Initial commit for html web/mobile app

---
+++ car_web/js/core/dataservice.js
@@ -0,0 +1,76 @@
+(function () {
+    /** This handles all interaction with the server.  
+    * @class app.core.dataservice
+    * @memberOf app.core    
+    */
+    angular
+        .module('app')
+        .factory('dataservice', dataservice); 
+
+    /* @ngInject */
+    function dataservice($http, $location, $q, userUrl, friendsUrl) {
+        var user = {};
+
+        var service = {
+            user: {
+                get: getUser,
+                getCache: getUserCache
+            },
+            friends: {
+                get: getFriends
+            }
+        };
+
+        return service;
+
+        function getFriends(){
+            return $http.get(buildUrl(friendsUrl), {
+                    params: {}
+                })
+                .then(getFriendsComplete)
+                .catch(function(message) {
+                    console.log(message);
+                });
+
+            function getFriendsComplete(data) {
+                var results = [];
+                if (data.data){
+                    results = data.data;
+                }
+
+                return results;
+            }
+        }
+
+        function getUserCache() {
+            return user;
+        }
+
+        function getUser(userId){
+            return $http.get(buildUrl(userUrl), {
+                    params: {}
+                })
+                .then(getUserComplete)
+                .catch(function(message) {
+                    console.log(message);
+                });
+
+            function getUserComplete(data) {
+                var results = [];
+                if (data.data){
+                    results = user = data.data;
+                }
+
+                return results;
+            }
+        }
+
+        function buildUrl(uri){
+            var url = '/' + uri;
+
+            
+
+            return url;
+        }
+    }
+})();
